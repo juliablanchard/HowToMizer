bd34904394da2a4691b44fd735f4e2503cc57702
Julia Blanchard <juliab6@DOM-IMA-6G5JHD2.local>
2020-07-29 15:47:39 +1000

    Added files

---
+++ shiny-equilibrium/server.R
@@ -0,0 +1,26 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(mizer)
+
+# Define server logic required to draw a plot
+shinyServer(function(input, output) {
+   
+  output$distPlot <- renderPlot({
+    # set up params using values given, need check and change parameter values so units work in days units 
+    params@species_params$erepro <- rep(10^input$erepro,12)
+   # params@species_params$Rmax <- rep(10^input$Rmax,12)
+    params <- setParams(params,kappa=10^input$kappa)
+    # run without fishing
+    sim <- project(params, effort = 1, t_max = 100)
+    plot(sim)
+     })
+
+})
