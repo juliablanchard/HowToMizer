0bc005708a44c0b15b6c23b774074de83abfd3ca
Romain Forestier <romain.forestier48@gmail.com>
2021-06-04 12:13:42 +1000

    getDietComp update

--- utility.R
+++ utility.R
@@ -541,88 +541,56 @@ plotCalibration <- function(sim, catch_dat = NULL, stage = 1, wlim = c(.1,NA), p
   return(p)
 }
 
-getDietComp<- function(sim, n = sim@n[dim(sim@n)[1],,],  n_pp = sim@n_pp[dim(sim@n_pp)[1],], 
-                       feedinglevel=getFeedingLevel(object, n = n,n_pp = n_pp)){
-  
-  # initialisation to get it working with latest Mizer version
+#' Get an array of predator species x predator mass x prey species x prey mass
+#' 
+#' @param sim An object of class \linkS4class{MizerSim}
+
+getDietComp<- function(sim)
+{
+  # initialisation
   object <- sim@params
+  feedinglevel=getFeedingLevel(object)
+  pred_kernel <- getPredKernel(object)
+  n = sim@n[dim(sim@n)[1],,]
+  n_pp = sim@n_pp[dim(sim@n_pp)[1],]
   no_sp <- dim(object@species_params)[1]
   no_w <- length(object@w)
   no_w_full <- length(object@w_full)
   
-  
-  pred_kernel <- array(0, dim = c(no_sp, no_w, no_w_full),
-                       dimnames = list(sp = object@species_params$species,
-                                       w_pred = signif(object@w, 3),
-                                       w_prey = signif(object@w_full, 3)))
   diet_comp<-array(0, c(no_sp, no_w, no_sp + 1, no_w_full),
                    dimnames=list( predator=as.character(object@species_params$species), pred_size = object@w,
                                   prey = c(as.character(object@species_params$species), "background"),
                                   prey_size = object@w_full))
   
-  # Filling pred_kernel  
-  
-  Beta <- log(object@species_params$beta)
-  sigma <- object@species_params$sigma
-  # w_full has the weights from the smallest relevant plankton, to the largest fish
-  x_full <- log(object@w_full)
-  # We choose the origin of the x axis to be at the smallest plankton size
-  x_full <- x_full - x_full[1]
-  dx <- x_full[2] - x_full[1]
-  # rr is the maximal log predator/prey mass ratio
-  rr <- Beta + 3 * sigma
-  ri <- floor(rr / dx)
-  # might need to add some initialisation here
-  # res@ft_pred_kernel_e <- matrix(0, nrow = no_sp, ncol = length(x_full))
-  for (i in 1:no_sp) {
-    # print(i)
-    # We compute the feeding kernel terms and their fft.
-    phi <- exp(-(x_full - Beta[i])^2 / (2 * sigma[i]^2))
-    phi[x_full > rr[i]] <- 0
-    phi[1] <- 0
-    # Fourier transform of feeding kernel for evaluating available energy
-    # res@ft_pred_kernel_e[i, ] <- fft(phi)
-    # Fourier transform of feeding kernel for evaluating predation rate
-    phi_p <- rep(0, no_w_full)
-    phi_p[(no_w_full - ri[i] + 1):no_w_full] <- phi[(ri[i] + 1):2]
-    # res@ft_pred_kernel_p[i, ] <- fft(phi_p)
-    # Full feeding kernel array
-    
-    min_w_idx <- no_w_full - no_w + 1
-    for (k in seq_len(no_w)) 
-      pred_kernel[i, k, (min_w_idx - 1 + k):1] <- phi[1:(min_w_idx - 1 + k)]
-    
-  }
-  
-  # Starting the function
-  # Biomass by species;
+  # Biomass by species
   n_total_in_size_bins<- sweep(n, 2, object@dw , "*")
   b_tot <- sweep(n_total_in_size_bins, 2, object@w , "*")
   
-  #Biomass of resource as prey; scaled to reflect pred size kernel; might have to change if we start using interaction with resource spectrum like Hartvig et al. 2011
+  # Index of predator size classes 
+  idx_sp<- object@w_full %in% object@w
   
-  #Note that we multiply the amount available by the availability parameter in the species parameter file 
-  b_background <- (sweep( pred_kernel[,,], c(3), object@dw_full*object@w_full*n_pp, "*")) 
+  # pred_kernel * interaction matrix
+  for(iW in 1:no_w){
+    for(iSpecies in 1:no_sp){    
+      diet_comp[iSpecies,iW,1:no_sp,idx_sp]<- sweep(sweep( b_tot, c(1), object@interaction[iSpecies, 1:no_sp], "*"), c(2),
+                                                    pred_kernel[iSpecies,iW,idx_sp], "*")
+    }
+  }
+  # Search rate *  feeding level * predator biomass
+  diet_comp[,,1:no_sp,]<- sweep(sweep(sweep(diet_comp[,,1:no_sp,], c(1,2), object@search_vol,"*"),
+                                      c(1,2),feedinglevel,"*"), 
+                                c(1,2),b_tot,"*")  # Prey eaten: total g prey/ year  (given predator biomass density)
+  
+  # no interaction matrix for background spectrum
+  b_background <- (sweep(pred_kernel[,,], c(3), object@dw_full*object@w_full*n_pp, "*")) 
   #Search rate *  feeding level * predator biomass
   b_background<- sweep(b_background, c(1,2), object@search_vol,"*") #Scale up by search volume
   b_background<- sweep(b_background, c(1,2), feedinglevel,"*") # Scale according to feeding level. Prey eaten: g prey / year / g predator
   b_background_tot<-sweep(b_background,c(1,2), b_tot, "*") # Prey eaten: total g prey/ year  (given predator biomass density)
-
-  # Index of predator size classes 
-  idx_sp<- object@w_full %in% object@w
-  
-  for(i in 1:no_w){
-    for(j in 1:no_sp){    
-      diet_comp[j,i,1:no_sp,idx_sp]<- sweep(sweep( b_tot, c(1), object@interaction[j, 1:no_sp], "*"), c(2), pred_kernel[j,i,idx_sp], "*")
-    }
-  }
-  
-  # Search rate *  feeding level * predator biomass
-  diet_comp[,,1:no_sp,]<- sweep(sweep(sweep(diet_comp[,,1:no_sp,], c(1,2), object@search_vol,"*"), c(1,2),feedinglevel,"*"), c(1,2),b_tot,"*")  # Prey eaten: total g prey/ year  (given predator biomass density)
   
   # Store background eaten 
   diet_comp[,,no_sp+1,]<- b_background_tot
-
+  
   return(diet_comp)
 } 
 
@@ -638,8 +606,8 @@ function (params, n = initialN(params), n_pp = initialNResource(params),
   no_w_full <- length(params@w_full)
   no_other <- length(params@other_encounter)
   other_names <- names(params@other_encounter)
-  assert_that(identical(dim(n), c(no_sp, no_w)), length(n_pp) == 
-                no_w_full)
+  # assert_that(identical(dim(n), c(no_sp, no_w)), length(n_pp) == 
+  #               no_w_full)
   diet <- array(0, dim = c(no_sp, no_w, no_sp + 1 + no_other), 
                 dimnames = list(predator = species, w = dimnames(params@initial_n)$w, 
                                 prey = c(as.character(species), "Resource", other_names)))
